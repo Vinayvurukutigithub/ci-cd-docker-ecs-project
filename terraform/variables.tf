b28b3f395d1ecd87a449140609f53ab438c15ce1
Vinayvurukutigithub <164153398+Vinayvurukutigithub@users.noreply.github.com>
2025-05-24 20:25:56 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,30 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "app_name" {
+  default = "nginx-app"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "desired_count" {
+  default = 1
+}
+
+variable "container_port" {
+  default = 80
+}
 
